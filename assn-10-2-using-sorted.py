4e82ab62aedc7230fff1ee25db8f1944a92e0482
wowjoepower <joepower0520@gmail.com>
2015-04-02 18:03:00 +0800

    2 versions for exercise 10.2

---
+++ assn-10-2-using-sorted.py
@@ -0,0 +1,21 @@
+#Use bulit-in function sorted() in tuple
+
+fname = raw_input("Enter file name: ")
+try:
+    if len(fname) == 0:
+        fname = "mbox-short.txt"
+    fhandle = open(fname)
+except:
+    print 'File cannot be opened:' , fname
+    exit()
+
+dic = dict()
+for line in fhandle:
+    if not line.startswith("From") or line.startswith("From:") : continue
+    time = line.split()[5]
+    hour = time.split(":")[0]
+    dic[hour] = dic.get(hour,0) + 1
+
+lst = list()
+for key, val in sorted(dic.items()):
+	print key,val
